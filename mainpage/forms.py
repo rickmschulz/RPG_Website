5359a91bf2d3cec89d66b9515949be2a2797a2c4
Henrique Schulz <henrique.mschulz@gmail.com>
2022-05-14 20:51:08 -0300

    Added Music

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -39,3 +39,23 @@ class InventoryForm(forms.ModelForm):
     #         raise forms.ValidationError('This is not a valid description!')
     #     return description
 
+
+# 1 - equipamento
+#  1.1 - armas
+#   1.1.1 - melee
+#   1.1.2 - ranged
+#  2.1 - armaduras
+#   2.1.1 - helmet
+#   2.1.2 - body-armor
+#   2.1.3 - acessórios
+#     2.1.1.1 - amuleto
+#     2.1.1.2 - anel
+#     2.1.1.3 - capas
+#     2.1.1.4 - braceletes
+#     2.1.1.5 - botas
+# 3 - consumíveis
+#   3.1 - poções
+#   3.2 - scrolls
+#   3.3 - varinhas
+#   3.4 - outros.
+#      3.4.1 - input livre
